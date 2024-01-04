4580118c957fe078d25bb87723d890594ce6bc27
HichamZineb <hichamelamine@hotmail.com>
2024-01-04 20:49:55 +0000

    update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 def pascal_triangle(n):
+    '''
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    '''
     if n <= 0:
         return []
     
